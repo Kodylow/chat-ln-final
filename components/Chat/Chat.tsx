a38eb8a567679116874e76afad4ff7efcd8e5246
Kody Low <kodylow7@gmail.com>
2023-05-07 23:10:17 -0500

    fix openai api key;

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -157,26 +157,15 @@ export const Chat: FC<Props> = memo(
         {!(apiKey || serverSideApiKeyIsSet) ? (
           <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
             <div className="text-center text-4xl font-bold text-black dark:text-white">
-              Welcome to Chatbot UI
+              Welcome to Chat-LN
             </div>
             <div className="text-center text-lg text-black dark:text-white">
-              <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-              <div className="mb-2 font-bold">
-                Important: Chatbot UI is 100% unaffiliated with OpenAI.
-              </div>
+              <div className="mb-8">{`Chat-LN is an open source clone of OpenAI's ChatGPT UI.`}</div>
             </div>
             <div className="text-center text-gray-500 dark:text-gray-400">
               <div className="mb-2">
-                Chatbot UI allows you to plug in your API key to use this UI
-                with their API.
-              </div>
-              <div className="mb-2">
-                It is <span className="italic">only</span> used to communicate
-                with their API.
-              </div>
-              <div className="mb-2">
                 {t(
-                  'Please set your OpenAI API key in the bottom left of the sidebar.',
+                  'Please set your OpenAI API key before deploying.',
                 )}
               </div>
               <div>
